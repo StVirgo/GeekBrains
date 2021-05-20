9419d7e0654a2989c7484edd983a3cb6ea8224d1
StVirgo <79658516+StVirgo@users.noreply.github.com>
2021-05-20 19:32:35 +0300

    HW_lvl3_lesson1
    HW_lvl3_lesson1

---
+++ HW_level3_lesson1/Apple.java
@@ -0,0 +1,9 @@
+package HW_level3_lesson1;
+
+public class Apple extends Fruit{
+    private float weightFruit = 1.0f;
+
+    public Apple() {
+        super(1.0f);
+    }
+}
---
+++ HW_level3_lesson1/Fruit.java
@@ -0,0 +1,12 @@
+package HW_level3_lesson1;
+
+public abstract class Fruit {
+    private float weightFruit;
+
+    public float getWeightFruit() {
+        return weightFruit;
+    }
+    public Fruit(float weightFruit) {
+        this.weightFruit = weightFruit;
+    }
+}
